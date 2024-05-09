6640ecd862422d94e76f787e27ef9eafd0e4a73a
Juan Rodriguez <rodriguezcolmenaresjuand@gmail.com>
2024-05-09 16:59:13 -0300

    Agregando Passport/local. Finalizado Desafio

--- src/routes/sessionRouter.js
+++ src/routes/sessionRouter.js
@@ -1,74 +1,39 @@
 import { Router } from 'express';
 import { UsuariosManagerDao } from '../dao/usuariosManagerDao.js';
-import { creaHash } from '../utils.js';
+import { creaHash, passportCall } from '../utils.js';
 import passport from 'passport';
 export const router=Router()
 
 let usuariosManager=new UsuariosManagerDao()
 
-router.get("/errorRegistro", (req, res)=>{
-    return res.redirect("/registro?error=Error al registrarse")
+router.get("/usuarios", async(req, res)=>{
+    let usuarios=await usuarioModelo.find().populate("rol").lean()
+
+    res.setHeader('Content-Type','application/json');
+    return res.status(200).json({usuarios});
 })
 
-router.post('/registro', passport.authenticate("registro", {failureRedirect:"/api/sessions/errorRegistro"}), async(req,res)=>{
+router.post("/registro", passportCall("registro"), (req, res)=>{
 
-    return res.redirect(`/registro?mensaje=Registro exitoso para ${req.user.nombre}`)
-
+    res.setHeader('Content-Type','application/json');
+    return res.status(201).json({status:"registro correcto", usuario:req.user});
 })
+
 
 router.get("/errorLogin", (req, res)=>{
     return res.status(400).json({error:`Error al logearse`})
 })
 
-router.post('/login', passport.authenticate("login", {failureRedirect:"/api/sessions/errorLogin"}), async(req,res)=>{
-// router.post('/registro',async(req,res)=>{
 
-//     let {nombre, email, password} =req.body
-//     if(!nombre || !email || !password){
-//         res.setHeader('Content-Type','application/json');
-//         return res.status(400).json({error:`Faltan datos`})
-//     } 
+router.post('/login',async(req,res)=>{
 
-//     let existe=await usuariosManager.getBy({email})
-//     if(existe){
-//         res.setHeader('Content-Type','application/json');
-//         return res.status(400).json({error:`Ya existen usuarios con email ${email}`})
-
-//     }
-//     password=creaHash(password)
-
-//     try {
-//         let nuevoUsuario=await usuariosManager.create({nombre, email, password})
-
-//         res.setHeader('Content-Type','application/json');
-//         return res.status(200).json({payload:"Registro exitoso", nuevoUsuario});
-
-//     } catch (error) {
-//         return res.status(500).json({error: `Error inesperado`})
-        
-//     }
+    let {email, password} =req.body
+    if(!email || !password){
+        res.setHeader('Content-Type','application/json');
+        return res.status(400).json({error:`Faltan datos`})
+    }
 
 
-// })
-
-// router.post('/login',async(req,res)=>{
-
-//     let {email, password} =req.body
-//     if(!email || !password){
-//         res.setHeader('Content-Type','application/json');
-//         return res.status(400).json({error:`Faltan datos`})
-//     }
-
-//     let usuario=await usuariosManager.getBy({email})
-//     if(!usuario){
-//         res.setHeader('Content-Type','application/json');
-//         return res.status(401).json({error:`Correo incorrecto`})
-//     }
-
-//     if(usuario.password!==creaHash(password)){
-//         res.setHeader('Content-Type','application/json');
-//         return res.status(401).json({error:`Contraseña incorrecta`})
-//     }
     let usuario=req.user
     usuario={...usuario}
     delete usuario.password
@@ -78,7 +43,7 @@ router.post('/login', passport.authenticate("login", {failureRedirect:"/api/sess
         // Asignar el rol de administrador al usuario
         usuario.rol = 'admin';
     }
-   
+
     res.setHeader('Content-Type','application/json')
     res.redirect('/productos')
 //     res.status(200).json({
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@ import {fileURLToPath} from 'url';
 import { dirname, join } from 'path';
 import crypto from "crypto"
 import bcrypt from 'bcrypt'
+import passport from 'passport';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,3 +15,20 @@ const SECRET="coderhouse"
 export const creaHash=password=>bcrypt.hashSync(password, bcrypt.genSaltSync(10))
 export const validaPassword=(usuario, password)=>bcrypt.compareSync(password, usuario.password)
 
+export const passportCall = (estrategia) => {
+    return function(req, res, next) {
+        passport.authenticate(estrategia, function(err, user, info, status) {
+            if (err) { 
+                return next(err); // Devolver el error al middleware de manejo de errores
+            }
+            if (!user) {
+                res.setHeader('Content-Type','application/json');
+                return res.status(401).json({
+                    error: info && info.message ? info.message : 'Error de autenticación'
+                });
+            }
+            req.user = user;
+            return next();
+        })(req, res, next);
+    };
+};
